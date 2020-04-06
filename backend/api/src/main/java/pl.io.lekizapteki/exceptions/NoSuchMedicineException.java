5f2ac879cdc33bc8ac7135793c51b3bd052a4bd6
Dawid Jamka <56138545+shadowatyyy@users.noreply.github.com>
2020-04-06 21:49:23 +0200

    B106 (#22)
    * dodane exception
    
    * dodane wiecej exception
    
    * statyczne pola na wiadomosci
    
    * private final static

---
+++ backend/api/src/main/java/pl.io.lekizapteki/exceptions/NoSuchMedicineException.java
@@ -0,0 +1,14 @@
+package pl.io.lekizapteki.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class NoSuchMedicineException extends RuntimeException {
+
+  private final static String message = "Nieprawidłowa jednostka chorobowa";
+
+  public NoSuchMedicineException() {
+    super(message);
+  }
+}
