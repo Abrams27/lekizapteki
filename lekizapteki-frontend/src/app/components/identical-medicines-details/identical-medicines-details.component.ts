d9104546ef38165cb30744e21eb6389a33b6aa7e
Abrams27 <abramowiczmarcin99@gmail.com>
2020-05-18 20:43:10 +0200

    naglowki i wyswietlanie glownych danych

--- lekizapteki-frontend/src/app/components/identical-medicines-details/identical-medicines-details.component.ts
+++ lekizapteki-frontend/src/app/components/identical-medicines-details/identical-medicines-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {animate, state, style, transition, trigger} from '@angular/animations';
+import {MedicineDetailsDto} from '../../services/webservices/models/medicine/detailed/medicine-details.dto';
+import {IdenticalMedicinesDetailsComponentProperties} from './identical-medicines-details.properties';
 
 @Component({
   selector: 'app-identical-medicines-details',
@@ -15,102 +17,73 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
 })
 export class IdenticalMedicinesDetailsComponent implements OnInit {
 
-  dataSource = ELEMENT_DATA;
-  columnsToDisplay = ['name', 'weight', 'symbol', 'position'];
-  expandedElement: PeriodicElement | null;
+  private dataSource = MEDICINE_DETAILS_MOCK;
+
+  private columnsToDisplay: string[] = [
+    IdenticalMedicinesDetailsComponentProperties.MEDICINE_NAME_HEADER,
+    IdenticalMedicinesDetailsComponentProperties.ACTIVE_INGREDIENT_HEADER,
+    IdenticalMedicinesDetailsComponentProperties.RETAIL_PRICE_HEADER
+  ];
+
+  expandedElement: MedicineDetailsDto | null;
 
   constructor() { }
 
   ngOnInit(): void { }
 
+  getDataSource(): MedicineDetailsDto[] {
+    return this.dataSource;
+  }
+
+  getColumnsToDisplay(): string[] {
+    return this.columnsToDisplay;
+  }
+
+  getColumnContentForHeader(header: string, element: MedicineDetailsDto): string | number {
+    if (header === IdenticalMedicinesDetailsComponentProperties.MEDICINE_NAME_HEADER) {
+      return element.name;
+    }
+
+    if (header === IdenticalMedicinesDetailsComponentProperties.ACTIVE_INGREDIENT_HEADER) {
+      return element.activeIngredient;
+    }
+
+    if (header === IdenticalMedicinesDetailsComponentProperties.RETAIL_PRICE_HEADER) {
+      return element.pricing.retailPrice;
+    }
+  }
+
 }
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-  description: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
+const MEDICINE_DETAILS_MOCK: MedicineDetailsDto[] = [
   {
-    position: 1,
-    name: 'Hydrogen',
-    weight: 1.0079,
-    symbol: 'H',
-    description: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
-        atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
-  }, {
-    position: 2,
-    name: 'Helium',
-    weight: 4.0026,
-    symbol: 'He',
-    description: `Helium is a chemical element with symbol He and atomic number 2. It is a
-        colorless, odorless, tasteless, non-toxic, inert, monatomic gas, the first in the noble gas
-        group in the periodic table. Its boiling point is the lowest among all the elements.`
-  }, {
-    position: 3,
-    name: 'Lithium',
-    weight: 6.941,
-    symbol: 'Li',
-    description: `Lithium is a chemical element with symbol Li and atomic number 3. It is a soft,
-        silvery-white alkali metal. Under standard conditions, it is the lightest metal and the
-        lightest solid element.`
-  }, {
-    position: 4,
-    name: 'Beryllium',
-    weight: 9.0122,
-    symbol: 'Be',
-    description: `Beryllium is a chemical element with symbol Be and atomic number 4. It is a
-        relatively rare element in the universe, usually occurring as a product of the spallation of
-        larger atomic nuclei that have collided with cosmic rays.`
-  }, {
-    position: 5,
-    name: 'Boron',
-    weight: 10.811,
-    symbol: 'B',
-    description: `Boron is a chemical element with symbol B and atomic number 5. Produced entirely
-        by cosmic ray spallation and supernovae and not by stellar nucleosynthesis, it is a
-        low-abundance element in the Solar system and in the Earth's crust.`
-  }, {
-    position: 6,
-    name: 'Carbon',
-    weight: 12.0107,
-    symbol: 'C',
-    description: `Carbon is a chemical element with symbol C and atomic number 6. It is nonmetallic
-        and tetravalent—making four electrons available to form covalent chemical bonds. It belongs
-        to group 14 of the periodic table.`
-  }, {
-    position: 7,
-    name: 'Nitrogen',
-    weight: 14.0067,
-    symbol: 'N',
-    description: `Nitrogen is a chemical element with symbol N and atomic number 7. It was first
-        discovered and isolated by Scottish physician Daniel Rutherford in 1772.`
-  }, {
-    position: 8,
-    name: 'Oxygen',
-    weight: 15.9994,
-    symbol: 'O',
-    description: `Oxygen is a chemical element with symbol O and atomic number 8. It is a member of
-         the chalcogen group on the periodic table, a highly reactive nonmetal, and an oxidizing
-         agent that readily forms oxides with most elements as well as with other compounds.`
-  }, {
-    position: 9,
-    name: 'Fluorine',
-    weight: 18.9984,
-    symbol: 'F',
-    description: `Fluorine is a chemical element with symbol F and atomic number 9. It is the
-        lightest halogen and exists as a highly toxic pale yellow diatomic gas at standard
-        conditions.`
-  }, {
-    position: 10,
-    name: 'Neon',
-    weight: 20.1797,
-    symbol: 'Ne',
-    description: `Neon is a chemical element with symbol Ne and atomic number 10. It is a noble gas.
-        Neon is a colorless, odorless, inert monatomic gas under standard conditions, with about
-        two-thirds the density of air.`
+    ean: 'EAN 1',
+    name: 'NAME 1',
+    activeIngredient: 'activeIngredient 1',
+    dose: 'dose 1',
+    form: 'form 1',
+    pricing: {
+      tradePrice: 1,
+      salePrice: 1,
+      retailPrice: 1,
+      totalFunding: 1,
+      chargeFactor: 1,
+      refund: 1
+    }
   },
+  {
+    ean: 'EAN 2',
+    name: 'NAME 2',
+    activeIngredient: 'activeIngredient 2',
+    dose: 'dose 2',
+    form: 'form 2',
+    pricing: {
+      tradePrice: 2,
+      salePrice: 2,
+      retailPrice: 2,
+      totalFunding: 2,
+      chargeFactor: 2,
+      refund: 2
+    }
+  }
 ];
