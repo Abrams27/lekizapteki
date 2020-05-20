50d42bc8fcb083e661d40f8b8a78f1026205d6b9
Abrams27 <abramowiczmarcin99@gmail.com>
2020-05-20 23:48:45 +0200

    wyszukiwanie lekow

--- lekizapteki-frontend/src/app/components/medicine-selection/medicine-selection.properties.ts
+++ lekizapteki-frontend/src/app/components/medicine-selection/medicine-selection.properties.ts
@@ -1,7 +1,7 @@
 export const MedicineSelectionProperties = {
 
   LIST_PLACEHOLDER: 'Wybierz lek',
-  INPUT_PLACEHOLDER: 'Podaj EAN',
+  INPUT_PLACEHOLDER: '  Podaj EAN',
   LIST_EMPTY_PLACEHOLDER: '',
   SEARCH_TYPE_EAN_BUTTON_NAME: 'Szukaj po EAN',
 
