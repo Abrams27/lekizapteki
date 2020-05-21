1aadc920a93373be4efb396bc48b672f9443a5a3
Dawid Jamka <56138545+shadowatyyy@users.noreply.github.com>
2020-05-21 10:32:01 +0200

    B107 (#46)
    * endpoint diseases
    
    * poprawki bez testow
    
    * ups bylo all args i utility jednoczesnie
    
    * usuniete components i dodane entery i dalej probuje dodac test
    
    * najwazniejszy test
    
    * poprawka testu
    
    * o, jakis include niepotrzebny

---
+++ lekizapteki-backend/app/src/main/java/pl/uw/mim/io/lekizapteki/services/DiseaseService.java
@@ -0,0 +1,18 @@
+package pl.uw.mim.io.lekizapteki.services;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.uw.mim.io.lekizapteki.repositories.DiseaseRepository;
+import pl.uw.mim.io.lekizapteki.repositories.entities.DiseaseEntity;
+
+@Service
+@AllArgsConstructor
+public class DiseaseService {
+
+  private DiseaseRepository diseaseRepository;
+
+  public List<DiseaseEntity> getAllDiseases() {
+    return diseaseRepository.findAll();
+  }
+}
--- lekizapteki-backend/app/src/main/java/pl/uw/mim/io/lekizapteki/usecases/GetDiseases.java
+++ lekizapteki-backend/app/src/main/java/pl/uw/mim/io/lekizapteki/usecases/GetDiseases.java
@@ -3,22 +3,17 @@ package pl.uw.mim.io.lekizapteki.usecases;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+import pl.uw.mim.io.lekizapteki.mappers.dto.DiseasesDtoMapper;
 import pl.uw.mim.io.lekizapteki.models.disease.DiseaseDto;
+import pl.uw.mim.io.lekizapteki.services.DiseaseService;
 
 @Component
 @AllArgsConstructor
 public class GetDiseases {
 
+  private DiseaseService diseaseService;
+
   public List<DiseaseDto> execute() {
-    // TODO TO JEST MOCK
-    return List.of(mockDisease());
+    return DiseasesDtoMapper.map(diseaseService.getAllDiseases());
   }
-
-  private DiseaseDto mockDisease() {
-    return DiseaseDto.builder()
-        .id(2137L)
-        .name("chuj")
-        .build();
-  }
-
 }
