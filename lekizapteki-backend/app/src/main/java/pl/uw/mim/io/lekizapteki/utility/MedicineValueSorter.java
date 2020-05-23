a640783162ec6a60714691d9ae812dea1fcdd496
Tomasz Patyna <45898941+Korpulentny@users.noreply.github.com>
2020-05-23 15:13:29 +0200

    B109 (#54)
    * Added MedicineValueSorterService
    
    * Poprawki
    
    * kosmetyczne poprawki
    
    Co-authored-by: Tomasz Patyna <tp406285@students.mimuw.edu.pl>

---
+++ lekizapteki-backend/app/src/main/java/pl/uw/mim/io/lekizapteki/utility/MedicineValueSorter.java
@@ -0,0 +1,72 @@
+package pl.uw.mim.io.lekizapteki.utility;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.experimental.UtilityClass;
+import pl.uw.mim.io.lekizapteki.repositories.entities.MedicineEntity;
+
+@UtilityClass
+public class MedicineValueSorter {
+
+  public List<MedicineEntity> sort(List<MedicineEntity> medicineEntityList) {
+    return medicineEntityList.stream()
+        .sorted(MedicineValueSorter::compareMedicineValue)
+        .collect(Collectors.toList());
+  }
+
+  private BigDecimal getDose(MedicineEntity medicineEntity) {
+    return new BigDecimal(getDoseValue(medicineEntity));
+  }
+
+  private BigDecimal getQuantity(MedicineEntity medicineEntity) {
+
+    return new BigDecimal(getQuantityValue(medicineEntity));
+  }
+
+  private String getDoseValue(MedicineEntity medicineEntity) {
+    return medicineEntity
+        .getDose()
+        .getDose()
+        .split(" ")[0];
+  }
+
+  private String getQuantityValue(MedicineEntity medicineEntity) {
+    return medicineEntity
+        .getPack()
+        .getContent()
+        .split(" ")[0];
+  }
+
+
+  private int compareMedicineValue(MedicineEntity firstMedicine, MedicineEntity secondMedicine) {
+    int result = getMultiplicationResult(firstMedicine, secondMedicine);
+    if (result == 0) {
+      result = getCompareResult(firstMedicine, secondMedicine);
+    }
+    return result;
+  }
+
+  private int getCompareResult(MedicineEntity firstMedicine, MedicineEntity secondMedicine) {
+    return secondMedicine.getName().compareTo(firstMedicine.getName());
+  }
+
+  private int getMultiplicationResult(MedicineEntity firstMedicine, MedicineEntity secondMedicine) {
+    return getMultiplication(secondMedicine, firstMedicine).compareTo(
+        getMultiplication(firstMedicine, secondMedicine));
+  }
+
+  private BigDecimal getMultiplication(MedicineEntity firstMedicine, MedicineEntity secondMedicine) {
+    return getRetailPrice(secondMedicine)
+        .multiply(calculateIngredientQuantity(firstMedicine));
+  }
+
+  private BigDecimal getRetailPrice(MedicineEntity secondMedicine) {
+    return secondMedicine.getPricing().getRetailPrice();
+  }
+
+  private BigDecimal calculateIngredientQuantity(MedicineEntity firstMedicine) {
+    return getDose(firstMedicine).multiply(getQuantity(firstMedicine));
+  }
+
+}
