df989c923fc481facc5d0c9815a13e136eea1788
Abrams27 <abramowiczmarcin99@gmail.com>
2020-05-23 18:02:47 +0200

    dodanie invokera i downloadera, przeniesienie rzeczy do propertisow + podbicie wercji poi

--- lekizapteki-backend/app/src/main/java/pl/uw/mim/io/lekizapteki/excel/parser/ParseExcelToDatabase.java
+++ lekizapteki-backend/app/src/main/java/pl/uw/mim/io/lekizapteki/excel/parser/ParseExcelToDatabase.java
@@ -2,22 +2,24 @@ package pl.uw.mim.io.lekizapteki.excel.parser;
 
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import pl.uw.mim.io.lekizapteki.excel.config.ExcelProcessingProperties;
 import pl.uw.mim.io.lekizapteki.excel.parser.models.Medicine;
 import pl.uw.mim.io.lekizapteki.excel.parser.validators.MedicinesValidator;
 import pl.uw.mim.io.lekizapteki.services.MedicineService;
 
 @Component
 @AllArgsConstructor
+@Slf4j
 public class ParseExcelToDatabase {
 
-  private final static String FILE_PATH = "src/main/resources/wykaz.xlsx"; // przeniesc do properties
-
+  private final ExcelProcessingProperties excelProcessingProperties;
   private final ExcelParser excelParser;
   private final MedicineService medicineService;
 
-  public void execute() {
-    List<Medicine> medicines = excelParser.parseExcelFile(FILE_PATH);
+  public void parse() {
+    List<Medicine> medicines = excelParser.parseExcelFile(excelProcessingProperties.getPath());
     List<Medicine> validatedMedicines = MedicinesValidator.filterAndParse(medicines);
 
     medicineService.saveMedicinesToRepository(validatedMedicines);
