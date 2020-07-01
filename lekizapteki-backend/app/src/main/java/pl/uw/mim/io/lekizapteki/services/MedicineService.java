0fe5bab4343335fbc33526c792443960e4d07657
Abrams27 <abramowiczmarcin99@gmail.com>
2020-07-02 00:46:23 +0200

    aktualizacja wyciagania identycznych

--- lekizapteki-backend/app/src/main/java/pl/uw/mim/io/lekizapteki/services/MedicineService.java
+++ lekizapteki-backend/app/src/main/java/pl/uw/mim/io/lekizapteki/services/MedicineService.java
@@ -1,13 +1,17 @@
 package pl.uw.mim.io.lekizapteki.services;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.uw.mim.io.lekizapteki.mappers.entity.MedicineEntityMapper;
 import pl.uw.mim.io.lekizapteki.excel.parser.models.Medicine;
 import pl.uw.mim.io.lekizapteki.exceptions.WrongMedicineForDiseaseException;
+import pl.uw.mim.io.lekizapteki.repositories.DiseaseRepository;
 import pl.uw.mim.io.lekizapteki.repositories.MedicineRepository;
 import pl.uw.mim.io.lekizapteki.repositories.entities.DiseaseEntity;
+import pl.uw.mim.io.lekizapteki.repositories.entities.IngredientEntity;
 import pl.uw.mim.io.lekizapteki.repositories.entities.MedicineEntity;
 
 @Service
@@ -17,9 +21,7 @@ public class MedicineService {
   private MedicineRepository medicineRepository;
   private DiseaseService diseaseService;
 
-//  private DoseRepository doseRepository;
-//  private DiseaseRepository diseaseRepository;
-//  private IngredientRepository ingredientRepository;
+  private DiseaseRepository diseaseRepository;
 
   public List<MedicineEntity> getMedicinesForDiseaseId(Long diseaseId) {
     DiseaseEntity diseaseEntity = diseaseService.getDiseaseWithIdOrThrow(diseaseId);
@@ -30,11 +32,30 @@ public class MedicineService {
 
   public List<MedicineEntity> getIdenticalMedicines(String ean, Long diseaseId) {
     MedicineEntity medicineEntity = getMedicineWithEanAndDiseaseIdOrThrow(ean, diseaseId);
+    Set<IngredientEntity> ingredientEntitySet = medicineEntity.getIngredients();
+
+    List<DiseaseEntity> diseaseEntityListWithName = diseaseRepository.findAllByName(medicineEntity.getDisease().getName());
 
     return medicineRepository
-        .findAllByDisease(medicineEntity.getDisease());
+        .findAllByDiseaseIn(diseaseEntityListWithName).stream()
+        .filter(o -> compareIngredients(o.getIngredients(), ingredientEntitySet))
+        .collect(Collectors.toList());
   }
 
+  private boolean compareIngredients(Set<IngredientEntity> givenIngredientEntities, Set<IngredientEntity> requiredIngredientEntities) {
+    return givenIngredientEntities.stream()
+        .allMatch(o -> isInRequired(o, requiredIngredientEntities));
+  }
+
+  private boolean isInRequired(IngredientEntity ingredientEntity, Set<IngredientEntity> requiredIngredientEntities) {
+    return requiredIngredientEntities.stream()
+        .anyMatch(o -> compareIngredient(o, ingredientEntity));
+  }
+
+  private boolean compareIngredient(IngredientEntity ingredientEntity1, IngredientEntity ingredientEntity2) {
+    return ingredientEntity1.getName().equals(ingredientEntity2.getName())
+        && ingredientEntity1.getDose().getDose().equals(ingredientEntity2.getDose().getDose());
+  }
   public MedicineEntity getMedicineWithEanAndDiseaseIdOrThrow(String ean, Long diseaseId) {
     DiseaseEntity diseaseEntity = diseaseService.getDiseaseWithIdOrThrow(diseaseId);
 
