ddb668661806b83647683b1828510d1b979212d7
Marcin Abramowicz <44381959+Abrams27@users.noreply.github.com>
2020-07-02 00:23:53 +0200

    fixes (#78)
    * tradeprice dodane
    
    * tradeprice w schema

--- lekizapteki-backend/app/src/main/java/pl/uw/mim/io/lekizapteki/excel/ExcelProcessingInvoker.java
+++ lekizapteki-backend/app/src/main/java/pl/uw/mim/io/lekizapteki/excel/ExcelProcessingInvoker.java
@@ -2,14 +2,12 @@ package pl.uw.mim.io.lekizapteki.excel;
 
 import javax.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import pl.uw.mim.io.lekizapteki.excel.downloader.ExcelDownloader;
 import pl.uw.mim.io.lekizapteki.excel.parser.ParseExcelToDatabase;
 
 @Component
 @AllArgsConstructor
-@Slf4j
 public class ExcelProcessingInvoker {
 
   private ExcelDownloader excelDownloader;
